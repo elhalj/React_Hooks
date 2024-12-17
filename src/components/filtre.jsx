8a26b18acac33453862a151900ada14143b751e3
elhalj <wilsonikedakoffi7@gmail.com>
2024-12-17 11:25:33 +0000

    ajout du composant filtre

--- src/components/filtre.jsx
+++ src/components/filtre.jsx
@@ -2,10 +2,11 @@
 
 
 
-function Filtre() {
+function Filtre({filtre, setFiltre}) {
     return (  
-        <div className="Filtre">
-            
+        <div>
+            <input type="text" placeholder="Filtrer par titre" value={filtre.titre} onChange={(e) => setFiltre({ ...filtre, titre: e.target.value})} />
+            <input type="number" placeholder="Filtrer par note" value={filtre.note} onChange={(e) => setFiltre({ ...filtre, note: e.target.value})} />
         </div>
     );
 }
--- src/components/movieListe.jsx
+++ src/components/movieListe.jsx
@@ -1,29 +1,55 @@
 import { useState } from "react";
 import MovieCard from "./movieCard";
-
+import Filtre from "./filtre";
 
 function MovieListe() {
     const [movies, setMovies] = useState([]);
+    const [filtre, setFiltre] = useState({ titre: "", note: "" });
 
     const addMovie = (movie) => {
-        setMovies([...movies, { ...movie, id: movies.length + 1}])
-    }
-    const afficher = () => {
-        movies.map(movie => {
-            return (
-                <div key={movie.id}>
-                    <h1>{movie.titre}</h1>
-                    <p>{movie.description}</p>
-                    <p>{movie.posterUrl}</p>
-                    <p>{movie.note}</p>
-                </div>
-            )
-        })
-    }
-    return (  
+        setMovies([...movies, { ...movie, id: movies.length + 1 }]);
+    };
+
+    const filtrerFilm = movies.filter((movie) => {
+        return (
+            (filtre.titre === "" ||
+                movie.titre.toLowerCase().include(filtre.titre.toLowerCase())) &&
+            (filtre.note === "" || movie.note >= Number(filtre.note))
+        );
+    });
+
+    //   const afficher = () => {};
+    return (
         <>
             <MovieCard onAddMovie={addMovie} />
-            {afficher()}
+            <div>
+                <h1>Filtre les Films</h1>
+                <Filtre filtre={filtre} setFiltre={setFiltre} />
+                <div>
+                    {filtrerFilm.map(movie => (
+                        <div key={movie.id}>
+                            <h2>{movie.id}</h2>
+                            <p>{movie.titre}</p>
+                            <p>{movie.posterUrl}</p>
+                            <p>Note: {movie.note}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <div>
+                {movies.map((movie) => {
+                    return (
+                        <div key={movie.id}>
+                            <h2>{movie.id}</h2>
+                            <h1>{movie.titre}</h1>
+                            <p>{movie.description}</p>
+                            <p>{movie.posterUrl}</p>
+                            <p>{movie.note}</p>
+                        </div>
+                    );
+                })}
+            </div>
         </>
     );
 }
